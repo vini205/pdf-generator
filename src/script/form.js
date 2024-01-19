87dd356dcc512b86e764f03e5eb970a695ce4851
Vinicius Gonçalves <vinicius.goncalves2007@gmail.com>
2024-01-19 19:15:21 -0300

    fix on toggle off

--- src/script/form.js
+++ src/script/form.js
@@ -1,5 +1,5 @@
 let tipoManutencao = getElement('itipo');
-campoPreventiva()
+campoCorretiva()
 tipoManutencao.addEventListener('change',()=>{
   if (tipoManutencao.value=='preventiva') {
     campoPreventiva()
